2659326c73d189706b95c95890874e818ec0ad05
Md salick rahman <salickrahman16@gmail.com>
2024-08-30 13:57:25 +0530

    Add files via upload

---
+++ Assignment_2/Assignment_2.js
@@ -0,0 +1,58 @@
+//1. adding two number
+let A = 400
+let B = 160
+let result = A + B
+console.log(result)
+
+//2.Condition are obeyed or not
+let a = 20
+let b = 40
+let condition = (a > b && a == b)
+console.log(condition)
+
+//3.to check divisibility
+let C = 25; 
+let D = 13;
+if (C % 10 === 0 && D % 10 === 0) {
+    console.log(true);
+}
+
+else if (C % 10 !== 0 && D % 10 !== 0) {
+    console.log(false);
+}
+
+else {
+    console.log(true);
+}
+
+//4. find first digit of four number
+let n= 4567;
+let firstDigit = n /1000
+console.log(Math.floor(n /1000));
+
+//5.last digit of four digit number
+let m= 4567;
+let lastDigit = m % 10
+console.log(Math.floor(n % 10));
+
+//6.getting remainder
+let x = 20
+let y = 2
+let remainder = x % y
+console.log(remainder)
+
+//7.multiply two number
+let salick = 44
+let rahman = 2
+let multiplication = (salick * rahman)
+console.log(multiplication)
+
+//8.marks calculator
+let English = 80
+let Hindi = 95
+let History = 88
+let totalMarks = (English + Hindi + History)
+console.log(totalMarks)
+let average = (totalMarks / 3)
+console.log(average)
+
